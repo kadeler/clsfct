a91c8b2cd3e8318346eeb22c5bacc0249872d354
kadeler <mensyoo2370@gmail.com>
2021-10-26 11:49:47 +0300

    Add files via upload

---
+++ clsfct/clsfct/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace clsfct
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настроитьКлючевыеСловаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.удалитьФайлИзСпискаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьСписокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.настройкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(336, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выбратьToolStripMenuItem
+            // 
+            this.выбратьToolStripMenuItem.Name = "выбратьToolStripMenuItem";
+            this.выбратьToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.выбратьToolStripMenuItem.Text = "Выбрать...";
+            this.выбратьToolStripMenuItem.Click += new System.EventHandler(this.chooseFileClickEvent);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.exitClickEvent);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настроитьКлючевыеСловаToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // настроитьКлючевыеСловаToolStripMenuItem
+            // 
+            this.настроитьКлючевыеСловаToolStripMenuItem.Name = "настроитьКлючевыеСловаToolStripMenuItem";
+            this.настроитьКлючевыеСловаToolStripMenuItem.Size = new System.Drawing.Size(227, 22);
+            this.настроитьКлючевыеСловаToolStripMenuItem.Text = "Настроить ключевые слова";
+            this.настроитьКлючевыеСловаToolStripMenuItem.Click += new System.EventHandler(this.tagsClickEvent);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 453);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(312, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Переименовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.renameClickEvent);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(312, 409);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listRightClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалитьФайлИзСпискаToolStripMenuItem,
+            this.очиститьСписокToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(207, 70);
+            // 
+            // удалитьФайлИзСпискаToolStripMenuItem
+            // 
+            this.удалитьФайлИзСпискаToolStripMenuItem.Name = "удалитьФайлИзСпискаToolStripMenuItem";
+            this.удалитьФайлИзСпискаToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.удалитьФайлИзСпискаToolStripMenuItem.Text = "Удалить файл из списка";
+            this.удалитьФайлИзСпискаToolStripMenuItem.Click += new System.EventHandler(this.eraseItemClickEvent);
+            // 
+            // очиститьСписокToolStripMenuItem
+            // 
+            this.очиститьСписокToolStripMenuItem.Name = "очиститьСписокToolStripMenuItem";
+            this.очиститьСписокToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.очиститьСписокToolStripMenuItem.Text = "Очистить список";
+            this.очиститьСписокToolStripMenuItem.Click += new System.EventHandler(this.clearListClickEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 488);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Классификатор новостных статей";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void chooseFileClickEvent(object sender, System.EventArgs e)
+        {
+            OpenFiles();
+        }
+
+        private void exitClickEvent(object sender, System.EventArgs e)
+        {
+            CloseApp();
+        }
+
+        private void tagsClickEvent(object sender, System.EventArgs e)
+        {
+            TagsOpenForm();
+        }
+
+        private void renameClickEvent(object sender, System.EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Вы не выбрали файлы для классификации", "Ошибка");
+            }
+            else
+            {
+            BeginRenameWork();
+            }
+        }
+
+        private void listRightClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                listBox1.SelectedIndex = listBox1.IndexFromPoint(e.X, e.Y);
+                if (listBox1.SelectedIndex != -1)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void updateListBox()
+        {
+            foreach (string path in filePaths)
+            {
+                this.listBox1.Items.AddRange(new object[] { path });
+            }
+            filePaths.Clear();
+        }
+
+        private void eraseItemClickEvent(object sender, System.EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void clearListClickEvent(object sender, System.EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настроитьКлючевыеСловаToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem удалитьФайлИзСпискаToolStripMenuItem;
+        private ToolStripMenuItem очиститьСписокToolStripMenuItem;
+    }
+}
+
---
+++ clsfct/clsfct/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace clsfct
+{
+    public partial class Form1 : Form
+    {
+        List<String> filePaths = new List<string>();
+        List<String[]> tags = new List<string[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("-------------------------------------------------");
+        }
+        public void OpenFiles()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Текстовые файлы (*.txt/*.pdf/*.docx/*.doc/*.html)|*.txt;*.pdf;*.docx;*.doc;*.html";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.Multiselect = true;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (String file in openFileDialog.FileNames)
+                    {
+                        if (!listBox1.Items.Contains(file))
+                        {
+                            filePaths.Add(file);
+                        }
+                    }
+                    /*foreach (String file in openFileDialog.FileNames)
+                    {
+                        filePath = openFileDialog.FileName;
+                        var fileStream = openFileDialog.OpenFile();
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            fileContent = reader.ReadToEnd();
+                        }
+                        Debug.WriteLine(filePath, fileContent);
+                    }*/
+                    updateListBox();
+                }
+            }
+        }
+
+        public void CloseApp()
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+        
+        public void TagsOpenForm()
+        {
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+        }
+
+        public void BeginRenameWork()
+        {
+            progressBar1.Visible = true;
+            button1.Visible = false;
+            progressBar1.Refresh();
+            progressBar1.Value = 16;
+            String resultfolder = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Desktop\\Классифицированные статьи";
+            if (!Directory.Exists(resultfolder))
+            {
+                Directory.CreateDirectory(resultfolder);
+            }
+            using (StreamReader reader = new StreamReader("config.txt"))
+            {
+                var line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("["))
+                    {
+                        tags.Add(line.Split("="));
+                    }
+                }
+            }
+            progressBar1.Value = 33;
+            foreach (String filepath in listBox1.Items)
+            {
+                if (filepath.Split(".").Last() == "pdf")
+                {
+                    var documenttag = "";
+                    String pdftext = "";
+                    StringBuilder text = new StringBuilder();
+                    using (PdfReader reader = new PdfReader(filepath))
+                    {
+                        for (int i = 1; i <= reader.NumberOfPages; i++)
+                        {
+                            text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                        }
+                    }
+                    pdftext = text.ToString();
+                    foreach (String[] array in tags)
+                    {
+                        documenttag = array[0];
+                        Debug.WriteLine("documenttag: " + documenttag);
+                        string[] tmp = array[1].Split(",");
+                        foreach (String word in tmp)
+                        {
+                            if (pdftext.Contains(word))
+                            {
+                                try
+                                {
+                                    File.Copy(filepath, resultfolder + "\\" + documenttag + filepath.Split("\\").Last());
+                                    Debug.WriteLine("Copied: " + filepath.Split("\\").Last() + " with documenttag:" + documenttag);
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+                }
+                else if (filepath.Split(".").Last() == "docx")
+                {
+                    var documenttag = "";
+                    String docxtext = "";
+                    using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(filepath, false))
+                    {  
+                        Body body = wordDocument.MainDocumentPart.Document.Body;
+                        docxtext = body.InnerText.ToString();
+                    }
+                    foreach (String[] array in tags)
+                    {
+                        documenttag = array[0];
+                        Debug.WriteLine("documenttag: " + documenttag);
+                        string[] tmp = array[1].Split(",");
+                        foreach (String word in tmp)
+                        {
+                            if (docxtext.Contains(word))
+                            {
+                                try
+                                {
+                                    File.Copy(filepath, resultfolder + "\\" + documenttag + filepath.Split("\\").Last());
+                                    Debug.WriteLine("Copied: " + filepath.Split("\\").Last() + " with documenttag:" + documenttag);
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    var documenttag = "";
+                    var line = "";
+                    using (StreamReader reader = new StreamReader(filepath))
+                    {
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            foreach (String[] array in tags)
+                            {
+                                documenttag = array[0];
+                                Debug.WriteLine("documenttag: " + documenttag);
+                                string[] tmp = array[1].Split(",");
+                                foreach (String word in tmp)
+                                {
+                                    if (line.Contains(word))
+                                    {
+                                        try
+                                        {
+                                            File.Copy(filepath, resultfolder + "\\" + documenttag + filepath.Split("\\").Last());
+                                            Debug.WriteLine("Copied: " + filepath.Split("\\").Last() + " with documenttag:" + documenttag);
+                                        }
+                                        catch { }
+                                    }
+                                }
+                                if (progressBar1.Value < 50)
+                                {
+                                    progressBar1.Value = 50;
+                                }
+
+                            }
+                            if (progressBar1.Value < 66)
+                            {
+                                progressBar1.Value = 66;
+                            }
+                        }
+                        if (progressBar1.Value < 83)
+                        {
+                            progressBar1.Value = 83;
+                        }
+                    }
+                }
+            }
+            progressBar1.Visible = false;
+            button1.Visible = true;
+            MessageBox.Show("Файлы классифицированы и скопированы в: "+resultfolder,"Готово");
+        }
+    }
+}
