57e9df727dd1f965f40aadf94d98e51afc64f325
kadeler <mensyoo2370@gmail.com>
2021-10-26 11:52:24 +0300

    Add files via upload

---
+++ clsfct/Form2.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+using System.IO;
+using System.Windows.Forms;
+
+namespace clsfct
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 251);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 276);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.Text = "Настройка кодовых слов";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.FormClosing += new FormClosingEventHandler(formClosingEvent);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void formClosingEvent(object sender, FormClosingEventArgs e)
+        {
+            using (StreamWriter reader = new StreamWriter("config.txt"))
+            {
+                reader.WriteLine(textBox1.Text);
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ clsfct/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace clsfct
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader("config.txt"))
+            {
+                textBox1.Text = reader.ReadToEnd();
+            }
+        }
+    }
+}
